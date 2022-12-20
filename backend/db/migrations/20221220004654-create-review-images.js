287b15fba25b54c82c544eda94a5c6f557238afb
Ruidan-Zhang <ruidanzhang973@gmail.com>
2022-12-19 16:55:14 -0800

    Table ReviewImages Created

---
+++ backend/db/migrations/20221220004654-create-review-images.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ReviewImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      reviewId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      url: {
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = "ReviewImages";
+    return queryInterface.dropTable(options);
+  }
+};
