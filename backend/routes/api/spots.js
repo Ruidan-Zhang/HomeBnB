b3b5c656fa2b98ea29a6452ee4c20da9026b729c
Ruidan-Zhang <ruidanzhang973@gmail.com>
2022-12-20 20:31:47 -0800

    get all spots pause

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,37 @@
+// backend/routes/api/spots.js
+const express = require('express');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { Spot, Review, SpotImage, sequelize } = require('../../db/models');
+
+const router = express.Router();
+
+//Get all Spots
+router.get('/', async (req, res) => {
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: SpotImage,
+                attributes: ['url', 'preview']
+            },
+            {
+                model: Review,
+                attributes: {
+                    include: [
+                        sequelize.fn('AVG', sequelize.col('stars')),
+                        'avgRating'
+                    ]
+                }
+            }
+        ]
+    });
+    const spotsArr = [];
+    spots.forEach(spot => {
+        spotsArr.push(spot);
+    });
+    return res.json({
+        "Spots": spotsArr
+    });
+})
+
+module.exports = router;
