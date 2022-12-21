a611d4598daeaa853860544648ead024f0aba06d
Ruidan-Zhang <ruidanzhang973@gmail.com>
2022-12-21 15:48:27 -0800

    edit a spot done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -234,4 +234,42 @@ router.post('/:spotId/images', requireAuth, async (req, res) => {
     }
 });
 
+//Edit a Spot
+router.put('/:spotId', validateSpot, requireAuth, async (req, res) => {
+    const spotId = req.params.spotId;
+    const foundSpot = await Spot.findByPk(spotId);
+
+    if (!foundSpot) {
+        res.status = 404;
+        res.statusCode = 404;
+        return res.json({
+            "message": "Spot couldn't be found",
+            "statusCode": res.status
+        })
+    } else if (req.user.id !== foundSpot.ownerId) {
+        res.status = 403;
+        res.statusCode = 403;
+        return res.json({
+            "message": "Forbidden",
+            "statusCode": res.status
+        })
+    } else {
+        const { address, city, state, country, lat, lng, name, description, price } = req.body;
+        foundSpot.update({
+            ownerId: req.user.id,
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price
+        });
+        return res.json(foundSpot);
+    }
+});
+
+
 module.exports = router;
