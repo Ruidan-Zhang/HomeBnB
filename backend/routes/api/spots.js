d7a5c998c246274266507b526d6e03e173556437
Ruidan-Zhang <ruidanzhang973@gmail.com>
2022-12-21 10:07:58 -0800

    get all spots done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -8,31 +8,41 @@ const router = express.Router();
 
 //Get all Spots
 router.get('/', async (req, res) => {
-    const spots = await Spot.findAll({
-        include: [
-            {
-                model: SpotImage,
-                attributes: ['url', 'preview']
-            },
-            {
-                model: Review,
-                attributes: {
-                    include: [
-                        sequelize.fn('AVG', sequelize.col('stars')),
-                        'avgRating'
-                    ]
-                }
-            }
-        ]
-    });
+    const spots = await Spot.findAll({raw: true});
     const spotsArr = [];
-    spots.forEach(spot => {
+
+    for (let spot of spots) {
+        let avgRating = await Review.findAll({
+            where: {
+                spotId: spot.id
+            },
+            attributes: [
+                [
+                    sequelize.fn('AVG', sequelize.col('stars')),
+                    'avgRating'
+                ]
+            ],
+            raw: true
+        });
+        avgRating = avgRating[0].avgRating;
+        spot.avgRating = avgRating;
+
+        let previewImage = await SpotImage.findAll({
+            where: {
+                spotId: spot.id
+            },
+            attributes: ['url']
+        });
+        previewImage = previewImage[0].url;
+        spot.previewImage = previewImage;
+
         spotsArr.push(spot);
-    });
+    }
+
     return res.json({
         "Spots": spotsArr
-    });
-})
+    })
+});
 
 //Get all Spots owned by the Current User
 
