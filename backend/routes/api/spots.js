ab4043e94f339f09ea54ccba89f63664ac14efbf
Ruidan-Zhang <ruidanzhang973@gmail.com>
2022-12-21 14:06:28 -0800

    create a spot pause

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,7 +4,41 @@ const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Spot, Review, SpotImage, User, sequelize } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
+
+const validateSpot = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage('Street address is required'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage('City is required'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .withMessage('State is required'),
+    check('country')
+      .exists({ checkFalsy: true })
+      .withMessage('Country is required'),
+    check('lat')
+      .exists({ checkFalsy: true })
+      .withMessage('Latitude is not valid'),
+      check('lng')
+      .exists({ checkFalsy: true })
+      .withMessage('Longitude is not validd'),
+    check('name')
+      .exists({ checkFalsy: true })
+      .withMessage('Name must be less than 50 characters'),
+    check('description')
+      .exists({ checkFalsy: true })
+      .withMessage('Description is required'),
+    check('price')
+      .exists({ checkFalsy: true })
+      .withMessage('Price per day is required'),
+    handleValidationErrors
+  ];
 
 //Get all Spots
 router.get('/', async (req, res) => {
@@ -141,6 +175,26 @@ router.get('/:spotId', async (req, res, next) => {
 
         return res.json(spot)
     }
+});
+
+//Create a Spot
+router.post('/', validateSpot, requireAuth, async (req, res) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    const newSpot = await Spot.create({
+        ownerId: req.user.id,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    });
+
+    res.status = 201;
+    return res.json(newSpot);
 });
 
 module.exports = router;
