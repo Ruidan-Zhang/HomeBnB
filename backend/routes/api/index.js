40cc3af71fbe209a40a423ae6a0ed6b8e18b82af
Ruidan-Zhang <ruidanzhang973@gmail.com>
2022-12-22 19:03:56 -0800

    Delete a Review Image done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,6 +6,7 @@ const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
 const spotimagesRouter = require('./spotimages.js');
+const reviewimagesRouter = require('./reviewimages.js');
 
 // GET /api/restore-user
 const { restoreUser } = require('../../utils/auth.js');
@@ -19,6 +20,7 @@ router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
 router.use('/spot-images', spotimagesRouter);
+router.use('/review-images', reviewimagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
