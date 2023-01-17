faa932c1d9fa4f924006d25185b500b65fe8ac0e
Ruidan-Zhang <ruidanzhang973@gmail.com>
2023-01-16 20:29:41 -0800

    load all spots done, styling needed

--- frontend/src/components/SingleSpotCard/index.js
+++ frontend/src/components/SingleSpotCard/index.js
@@ -1,13 +1,17 @@
 import './SingleSpotCard.css';
 
-const SingleSpotCard = ({id, city, state, price, avaRating, previewImage}) => {
+const SingleSpotCard = ({id, city, state, price, avgRating, previewImage}) => {
 
     return (
         <div className="single-spot-card-container">
-            <img className='single-spot-previewImage' src={previewImage} alt='single-spot-image'/>
-            <div className='single-spot-rating'>avaRating???</div>
-            <div className="single-spot-card-address">{city}, {state}</div>
-            <div className="single-spot-card-price">{price}</div>
+            <img className='single-spot-previewImage' src={previewImage} alt='single-spot-preview'/>
+            <div className='single-spot-address-and-rating'>
+                <div className="single-spot-card-address">{city}, {state}</div>
+                <div className='single-spot-rating'>
+                <i class="fa-solid fa-star"></i> {avgRating}
+                </div>
+            </div>
+            <div className="single-spot-card-price">${price} night</div>
         </div>
     )
 };
