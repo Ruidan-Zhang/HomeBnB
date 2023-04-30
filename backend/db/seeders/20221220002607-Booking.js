2bf5cd76ffdd42b2033be8c455d6a8404487b44b
Ruidan-Zhang <ruidanzhang973@gmail.com>
2023-04-30 14:40:10 -0700

    Added more seeders and a footer

--- backend/db/seeders/20221220002607-Booking.js
+++ backend/db/seeders/20221220002607-Booking.js
@@ -11,28 +11,70 @@ module.exports = {
     options.tableName = 'Bookings';
     return queryInterface.bulkInsert(options, [
       {
-        spotId: 1,
-        userId: 1,
-        startDate: '2000-10-04',
-        endDate: '2000-10-06'
-      },
-      {
         spotId: 2,
-        userId: 2,
-        startDate: '2001-12-28',
-        endDate: '2001-12-30'
+        userId: 1,
+        startDate: '2021-10-04',
+        endDate: '2021-10-06'
       },
       {
         spotId: 3,
-        userId: 3,
-        startDate: '2002-01-24',
-        endDate: '2002-01-26'
+        userId: 1,
+        startDate: '2021-12-28',
+        endDate: '2021-12-30'
       },
       {
-        spotId: 1,
-        userId: 2,
+        spotId: 4,
+        userId: 1,
+        startDate: '2023-07-30',
+        endDate: '2023-08-05'
+      },
+      {
+        spotId: 5,
+        userId: 1,
         startDate: '2023-05-10',
         endDate: '2023-05-15'
+      },
+      {
+        spotId: 6,
+        userId: 1,
+        startDate: '2023-09-16',
+        endDate: '2023-10-13'
+      },
+      {
+        spotId: 7,
+        userId: 1,
+        startDate: '2023-08-11',
+        endDate: '2023-09-06'
+      },
+      {
+        spotId: 8,
+        userId: 1,
+        startDate: '2023-12-09',
+        endDate: '2023-12-18'
+      },
+      {
+        spotId: 9,
+        userId: 1,
+        startDate: '2023-06-23',
+        endDate: '2023-07-28'
+      },
+      {
+        spotId: 10,
+        userId: 1,
+        startDate: '2024-01-03',
+        endDate: '2024-02-19'
+      },
+      {
+        spotId: 11,
+        userId: 1,
+        startDate: '2024-03-08',
+        endDate: '2024-03-30'
+      },
+      {
+        spotId: 12,
+        userId: 1,
+        startDate: '2024-04-17',
+        endDate: '2024-05-07'
       }
     ], {});
   },
@@ -40,8 +82,6 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     options.tableName = 'Bookings';
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2, 3] }
-    }, {});
+    return queryInterface.bulkDelete(options, {}, {});
   }
 };
---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import "./Footer.css"
+
+const Footer = () =>{
+    return (
+        <div className='footer-main-container'>
+          <div>
+            Ruidan Zhang © 2023 <a href = "https://github.com/Ruidan-Zhang" target="_blank" className='log-in-footer-icon' rel="noopener noreferrer"><i class="fa-brands fa-github"></i> </a>
+            <a href = "https://www.linkedin.com/in/ruidan-meredith-zhang/" target="_blank" className='log-in-footer-icon' rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+          </div>
+          <div>
+            Javascript | Express | React | Redux | SqlAlchemy | PostgresSQL | HTML | CSS | AWS | Hosted on Render
+          </div>
+        </div>
+    )
+};
+
+export default Footer;
