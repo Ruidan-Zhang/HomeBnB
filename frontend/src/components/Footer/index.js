7e756737d584d4393344018890a8222db9daf5cd
Ruidan-Zhang <ruidanzhang973@gmail.com>
2023-07-30 22:02:30 -0700

    another github testing

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -3,7 +3,6 @@ import "./Footer.css"
 const Footer = () =>{
     return (
         <div className='footer-main-container'>
-          {/* testing */}
           <div>
             Ruidan Zhang © 2023 <a href = "https://github.com/Ruidan-Zhang" target="_blank" className='log-in-footer-icon' rel="noopener noreferrer"><i class="fa-brands fa-github"></i> </a>
             <a href = "https://www.linkedin.com/in/ruidan-meredith-zhang/" target="_blank" className='log-in-footer-icon' rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
